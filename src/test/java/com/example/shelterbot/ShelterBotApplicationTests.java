00505affeeaae3f942f03454ab6d54d496bae9f2
sergeybryan <regkmastis@yandex.ru>
2023-05-28 09:44:13 +0300

    Initial commit

---
+++ src/test/java/com/example/shelterbot/ShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shelterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
