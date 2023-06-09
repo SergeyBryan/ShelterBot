b70e08abfc2442388eb0d3d85b23b6feae438a56
mamedovaesmira <mamedovaesmira88@gmail.com>
2023-06-09 23:15:57 +0300

    Создала классы репозиториев для таблиц Dog_Shelter, Cat_Shelter, Cats, Dogs, User, Volunteer

---
+++ src/main/java/com/example/shelterbot/repository/CatsRepository.java
@@ -0,0 +1,9 @@
+package com.example.shelterbot.repository;
+import com.example.shelterbot.model.Cats;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CatsRepository extends JpaRepository<Cats, Integer> {
+
+}
